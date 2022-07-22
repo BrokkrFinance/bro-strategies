ccc3aea287981ef27e8519bd3b6f8b9a2ee693f0
Daniel Graczer <daniel.graczer@block42.com>
2022-07-22 09:50:06 +0700

    bumping up solc version to 0.8.10

--- hardhat-coverage.config.ts
+++ hardhat-coverage.config.ts
@@ -13,7 +13,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.9",
+        version: "0.8.10",
         settings: {
           viaIR: false,
           optimizer: {
