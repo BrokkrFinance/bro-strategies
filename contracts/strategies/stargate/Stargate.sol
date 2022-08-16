c08a0e97ee39bc9e175e9efab92d6b0ba4c576dd
Jihoon Song <jihoonsong.dev@gmail.com>
2022-08-16 10:34:50 +0200

    Set contracts to interact during initialization

--- contracts/strategies/stargate/Stargate.sol
+++ contracts/strategies/stargate/Stargate.sol
@@ -2,10 +2,15 @@
 pragma solidity ^0.8.0;
 
 import "../../common/bases/StrategyOwnablePausableBaseUpgradeable.sol";
+import "../../dependencies/stargate/IStargateLpStaking.sol";
+import "../../dependencies/stargate/IStargatePool.sol";
+import "../../dependencies/stargate/IStargateRouter.sol";
 
 contract Stargate is StrategyOwnablePausableBaseUpgradeable {
     using SafeERC20Upgradeable for IInvestmentToken;
     using SafeERC20Upgradeable for IERC20Upgradeable;
+
+    error InvalidStargateLpToken();
 
     // solhint-disable-next-line const-name-snakecase
     string public constant name =
@@ -15,12 +20,50 @@ contract Stargate is StrategyOwnablePausableBaseUpgradeable {
     // solhint-disable-next-line const-name-snakecase
     string public constant version = "1.0.0";
 
+    IStargateRouter public stargateRouter;
+    IStargatePool public stargatePool;
+    IStargateLpStaking public stargateLpStaking;
+    IERC20Upgradeable public stargateDepositToken;
+    IERC20Upgradeable public stargateLpToken;
+    IERC20Upgradeable public stargateStgToken;
+    uint256 public stargatePoolId;
+    uint256 public stargateFarmId;
+
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
     }
 
+    function initialize(
+        StrategyArgs calldata strategyArgs,
+        IStargateRouter stargateRouter_,
+        IStargatePool stargatePool_,
+        IStargateLpStaking stargateLpStaking_,
+        IERC20Upgradeable stargateLpToken_,
+        IERC20Upgradeable stargateStgToken_
+    ) external initializer {
         __StrategyOwnablePausableBaseUpgradeable_init(strategyArgs);
+        stargateRouter = stargateRouter_;
+        stargatePool = stargatePool_;
+        stargateLpStaking = stargateLpStaking_;
+        stargateLpToken = stargateLpToken_;
+        stargateStgToken = stargateStgToken_;
+
+        stargateDepositToken = IERC20Upgradeable(stargatePool.token());
+        stargatePoolId = stargatePool.poolId();
+
+        IStargateLpStaking.PoolInfo memory poolInfo;
+        uint256 poolLength = stargateLpStaking.poolLength();
+        for (uint256 i = 0; i < poolLength; i++) {
+            poolInfo = stargateLpStaking.poolInfo(i);
+            if (address(poolInfo.lpToken) == address(stargateLpToken)) {
+                stargateFarmId = i;
+                break;
+            }
+        }
+        if (address(poolInfo.lpToken) != address(stargateLpToken)) {
+            revert InvalidStargateLpToken();
+        }
     }
 
     function _deposit(uint256 amount, NameValuePair[] calldata)
