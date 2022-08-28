ced430a48bf9a9fd4441b4683df57ff4373cb156
Jihoon Song <jihoonsong.dev@gmail.com>
2022-08-29 07:05:50 +0900

    Implement unified ERC165 test

--- test/strategy/UnifiedERC165.test.ts
+++ test/strategy/UnifiedERC165.test.ts
@@ -1,4 +1,13 @@
+import { expect } from "chai"
+
 export function testERC165() {
   describe("ERC165", async function () {
+    it("should success to support all interfaces that strategy inherits", async function () {
+      expect(await this.strategy.supportsInterface("0x49147370")).to.equal(true) // IAum
+      expect(await this.strategy.supportsInterface("0x52f3b8ca")).to.equal(true) // IFee
+      expect(await this.strategy.supportsInterface("0xa9233731")).to.equal(true) // IInvestable
+      expect(await this.strategy.supportsInterface("0xb7ac895f")).to.equal(true) // IReward
+      expect(await this.strategy.supportsInterface("0x00000000")).to.equal(true) // IStrategy
+    })
   })
 }
