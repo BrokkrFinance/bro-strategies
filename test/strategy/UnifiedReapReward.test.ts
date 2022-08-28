6864a8e0c3fc0989e380d752ca91f872263709f3
Jihoon Song <jihoonsong.dev@gmail.com>
2022-08-29 07:06:33 +0900

    Implement unified reap reward test

--- test/strategy/UnifiedReapReward.test.ts
+++ test/strategy/UnifiedReapReward.test.ts
@@ -1,4 +1,17 @@
+import { expect } from "chai"
+import { airdropToken } from "../shared/utils"
+
 export function testReapReward() {
   describe("ReapReward", async function () {
+    it("should success when any user processes reward", async function () {
+      const usdcBalance = await this.usdc.balanceOf(this.impersonatedSigner.address)
+
+      airdropToken(this.impersonatedSigner, this.user0, this.usdc, usdcBalance)
+
+      await this.usdc.connect(this.user0).approve(this.strategy.address, usdcBalance)
+      await this.strategy.connect(this.user0).deposit(usdcBalance, this.user0.address, [])
+
+      await expect(this.strategy.connect(this.user1).processReward([], [])).to.emit(this.strategy, "RewardProcess")
+    })
   })
 }
