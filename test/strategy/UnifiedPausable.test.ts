ab9b68c21bde72552681f74da44cfd6529d5e112
Jihoon Song <jihoonsong.dev@gmail.com>
2022-08-29 22:04:21 +0900

    Add withdraw reward test when a strategy is paused to UnifiedPausable

--- test/strategy/UnifiedPausable.test.ts
+++ test/strategy/UnifiedPausable.test.ts
@@ -36,5 +36,18 @@ export function testPausable() {
 
       expect(await this.usdc.balanceOf(this.user0.address)).to.equal(ethers.utils.parseUnits("70", 6))
     })
+
+    it("should fail when any user withdraws reward and the strategy is paused", async function () {
+      airdropToken(this.impersonatedSigner, this.user0, this.usdc, ethers.utils.parseUnits("100", 6))
+
+      await this.usdc.connect(this.user0).approve(this.strategy.address, ethers.utils.parseUnits("30", 6))
+      await this.strategy.connect(this.user0).deposit(ethers.utils.parseUnits("30", 6), this.user0.address, [])
+
+      expect(await this.strategy.pause()).not.to.be.reverted
+
+      await expect(this.strategy.connect(this.user0).withdrawReward([])).to.be.revertedWith("Pausable: paused")
+
+      expect(await this.usdc.balanceOf(this.user0.address)).to.equal(ethers.utils.parseUnits("70", 6))
+    })
   })
 }
