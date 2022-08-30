7ab593a24c0f462bbe1db76b3130cfb5c99a1748
Jihoon Song <jihoonsong.dev@gmail.com>
2022-08-31 01:26:54 +0900

    Add TraderJoe interfaces

---
+++ contracts/dependencies/traderjoe/ITraderJoeMasterChef.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface ITraderJoeMasterChef {
+    struct PoolInfo {
+        IERC20 lpToken;
+        uint256 allocPoint;
+        uint256 lastRewardTimestamp;
+        uint256 accJoePerShare;
+    }
+
+    struct UserInfo {
+        uint256 amount;
+        uint256 rewardDebt;
+    }
+
+    function poolLength() external view returns (uint256);
+
+    function poolInfo(uint256 pid)
+        external
+        view
+        returns (ITraderJoeMasterChef.PoolInfo memory);
+
+    function userInfo(uint256 _poolId, address _user)
+        external
+        view
+        returns (UserInfo memory);
+
+    function deposit(uint256 _pid, uint256 _amount) external;
+
+    function withdraw(uint256 pid, uint256 amount) external;
+}
