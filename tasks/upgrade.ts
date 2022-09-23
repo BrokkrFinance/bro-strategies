8471fedaeeb3cd9569df1bbb5ebee18a3f5cbb70
Jihoon Song <jihoonsong.dev@gmail.com>
2022-09-23 15:12:49 +0900

    Take multisig as a parameter

--- tasks/upgrade.ts
+++ tasks/upgrade.ts
@@ -3,14 +3,15 @@ import { task } from "hardhat/config"
 task("upgrade", "Upgrade a proxy contract to point to a new implementation contract")
   .addParam("proxy", "an address of proxy contract to be upgraded")
   .addParam("newImplementation", "the name of new implementation contract")
+  .addParam("multisig", "an address of multisig to propose an upgrade")
   .setAction(async (taskArgs, hre) => {
     console.log(
-      `Preparing upgrade proposal of proxy ${taskArgs.proxy} with new implementation ${taskArgs.newImplementation}`
+      `Preparing upgrade proposal of proxy ${taskArgs.proxy} with new implementation ${taskArgs.newImplementation} to ${taskArgs.multisig}`
     )
 
     const NewImplementation = await hre.ethers.getContractFactory(taskArgs.newImplementation)
     const proposal = await hre.defender.proposeUpgrade(taskArgs.proxy, NewImplementation, {
-      multisig: "0xE8855828fEC29dc6860A4362BCb386CCf6C0c601",
+      multisig: taskArgs.multisig,
     })
 
     console.log(`The upgrade proposal is created at ${proposal.url}\n`)
