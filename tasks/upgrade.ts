b8290d8e62ece81b7229ead5ec10b389018685cd
Jihoon Song <jihoonsong.dev@gmail.com>
2022-09-27 01:19:38 +0900

    Verify new implementation contract after upgrade proposal is created

--- tasks/upgrade.ts
+++ tasks/upgrade.ts
@@ -1,12 +1,12 @@
 import { task } from "hardhat/config"
 
 task("upgrade", "Upgrade a proxy contract to point to a new implementation contract")
-  .addParam("proxy", "an address of proxy contract to be upgraded")
-  .addParam("newImplementation", "the name of new implementation contract")
-  .addParam("multisig", "an address of multisig to propose an upgrade")
+  .addParam("proxy", "An address of proxy contract to be upgraded")
+  .addParam("newImplementation", "The name of new implementation contract")
+  .addParam("multisig", "An address of multisig to propose an upgrade")
   .setAction(async (taskArgs, hre) => {
     console.log(
-      `Preparing upgrade proposal of proxy ${taskArgs.proxy} with new implementation ${taskArgs.newImplementation} to ${taskArgs.multisig}`
+      `Prepare upgrade proposal of proxy ${taskArgs.proxy} with new implementation ${taskArgs.newImplementation} to ${taskArgs.multisig}`
     )
 
     const NewImplementation = await hre.ethers.getContractFactory(taskArgs.newImplementation)
@@ -15,4 +15,23 @@ task("upgrade", "Upgrade a proxy contract to point to a new implementation contr
     })
 
     console.log(`The upgrade proposal is created at ${proposal.url}\n`)
+
+    console.log("Verify new implementation contract if it is unverified")
+
+    const newImplementationAddress = proposal.metadata?.newImplementationAddress
+    if (newImplementationAddress !== undefined) {
+      try {
+        await hre.run("verify", { address: newImplementationAddress })
+      } catch (e: unknown) {
+        if (e instanceof Error && e.message === "Contract source code already verified") {
+          console.log("New implementation contract is already verified.")
+        } else {
+          console.log(`An error occured during verificaiton: ${e}`)
+        }
+      }
+    } else {
+      console.log("Couldn't find new implementation contract's address. Skip verification.")
+    }
+
+    console.log()
   })
