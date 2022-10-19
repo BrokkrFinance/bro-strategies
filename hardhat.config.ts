8d672bffec228c496cdafca04909a8d9402f5f6e
Jihoon Song <jihoonsong.dev@gmail.com>
2022-10-19 17:48:28 +0900

    Modify block number to fork to the recent one

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -48,7 +48,7 @@ const config: HardhatUserConfig = {
       forking: {
         url: "https://api.avax.network/ext/bc/C/rpc",
         enabled: true,
-        blockNumber: 18191781,
+        blockNumber: 21250000,
       },
     },
 
