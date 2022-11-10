47117bc99cadad10096a932b78adc691d3efae17
nnoln <mark3dom@mail.ru>
2022-11-11 00:03:19 +0100

    dca cutted; deployment script

---
+++ scripts/deploy.dca.ts
@@ -0,0 +1,43 @@
+import { Contract } from "ethers"
+import { deployProxyContract, getUsdcContract, logBlue, retryUntilSuccess } from "./helper"
+
+import portfolioConfig from "../configs/dca/portfolio/PercAllocation.json"
+import wBtcConfig from "../configs/dca/strategy/WBTC.json"
+
+let usdc: Contract
+
+async function deployWBTCDcaStrategy(): Promise<Contract> {
+  return await deployProxyContract("WBTCBluechip", [
+    wBtcConfig.dcaBaseArgs,
+    wBtcConfig.bluechipTokenInfo,
+    wBtcConfig.platypusInfo,
+    wBtcConfig.ptpIntoBluechipSwapPath,
+    wBtcConfig.avaxIntoBluechipSwapPath,
+  ])
+}
+
+async function deployDcaPortfolio(): Promise<Contract> {
+  return await deployProxyContract("PercentageAllocationPortfolio", [portfolioConfig.initArgs])
+}
+
+async function main() {
+  usdc = await getUsdcContract()
+
+  let dcaWBTC = await retryUntilSuccess(deployWBTCDcaStrategy())
+  portfolioConfig.initArgs.investables = [dcaWBTC.address] as never[]
+
+  let dcaPortfolio = await retryUntilSuccess(deployDcaPortfolio())
+
+  await retryUntilSuccess(dcaWBTC.addPortfolio(dcaPortfolio.address))
+
+  await retryUntilSuccess(dcaWBTC.transferOwnership(wBtcConfig.owner))
+  await retryUntilSuccess(dcaPortfolio.transferOwnership(portfolioConfig.owner))
+
+  logBlue(`WBTC DCA Deplyed at: ${dcaWBTC.address}`)
+  logBlue(`DCA Portfolio Deployed at ${dcaPortfolio.address}`)
+}
+
+main().catch((error) => {
+  console.error(error)
+  process.exitCode = 1
+})
