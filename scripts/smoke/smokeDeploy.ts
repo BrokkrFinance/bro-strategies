b3a9f44b9207b79eb01b6cecd657fa94a4de89a4
Daniel Graczer <daniel.graczer@block42.com>
2022-11-18 16:23:01 +0700

    Added prod deployment script for dns

--- scripts/smoke/smokeDeploy.ts
+++ scripts/smoke/smokeDeploy.ts
@@ -41,8 +41,13 @@ describe("DNS Vector deployment", function () {
 })
 
 async function getUsdcPortfolio() {
-  const SingleOwnerAddress = "0xFf04026EaBb113A04878F8861902e533A51feb22"
-  const OwnerAddrs = [SingleOwnerAddress, "0xeb1BF79Dfd7197308Ea045094217Bfc3c6aB9384"]
+  const singleOwnerAddress = "0xE8855828fEC29dc6860A4362BCb386CCf6C0c601"
+  const maintainerRoleAddress = "0x44041eE3A2ae30a1F2a79710542040d9C5BeEb2c"
+  const feeReceiver = "0xD81965d1D44c084b43fBFE1Cc5baC7414cd4FbbD"
+  const swapServiceAddress = "0x60aE616a2155Ee3d9A68541Ba4544862310933d4"
+  const aaveOracleAddress = "0x736d964c70fF405e14aAc3094CeFe585812e1877"
+
+  const ownerAddrs = [singleOwnerAddress]
 
   const usdcContract = await expectSuccess(getUsdcContract())
 
@@ -61,33 +66,36 @@ async function getUsdcPortfolio() {
   let dnsStrategy = await retryUntilSuccess(
     deployUpgradeableStrategy(
       "DnsVectorStrategy",
-      "Super Strategy Token 2",
-      "SUP2",
+      "USDC-AVAX Delta Neutral",
+      "DN1",
       usdcContract,
       0,
       [],
-      0,
+      500,
       [],
       0,
       [],
-      "0xce70b9444c4e22ae150C81dA7375542B49D15efA", // fee receiver
+      feeReceiver, // fee receiver
       [],
       BigInt(10 ** 20),
       BigInt(10 ** 20),
-      "0x736d964c70fF405e14aAc3094CeFe585812e1877", // Aave oracle
+      aaveOracleAddress, // Aave oracle
       0,
-      "0x60aE616a2155Ee3d9A68541Ba4544862310933d4", // swap service address
+      swapServiceAddress, // swap service address
       [
-        { role: "0x0000000000000000000000000000000000000000000000000000000000000000", users: OwnerAddrs },
-        { role: "0x7935bd0ae54bc31f548c14dba4d37c5c64b3f8ca900cb468fb8abd54d5894f55", users: OwnerAddrs },
-        { role: "0x17a8e30262c1f919c33056d877a3c22b95c2f5e4dac44683c1c2323cd79fbdb0", users: OwnerAddrs },
-        { role: "0x339759585899103d2ace64958e37e18ccb0504652c81d4a1b8aa80fe2126ab95", users: OwnerAddrs },
-        { role: "0x88aa719609f728b0c5e7fb8dd3608d5c25d497efbb3b9dd64e9251ebba101508", users: OwnerAddrs },
-        { role: "0x139c2898040ef16910dc9f44dc697df79363da767d8bc92f2e310312b816e46d", users: OwnerAddrs },
+        { role: "0x0000000000000000000000000000000000000000000000000000000000000000", users: ownerAddrs },
+        { role: "0x7935bd0ae54bc31f548c14dba4d37c5c64b3f8ca900cb468fb8abd54d5894f55", users: ownerAddrs },
+        { role: "0x17a8e30262c1f919c33056d877a3c22b95c2f5e4dac44683c1c2323cd79fbdb0", users: ownerAddrs },
+        {
+          role: "0x339759585899103d2ace64958e37e18ccb0504652c81d4a1b8aa80fe2126ab95",
+          users: [...ownerAddrs, maintainerRoleAddress],
+        },
+        { role: "0x88aa719609f728b0c5e7fb8dd3608d5c25d497efbb3b9dd64e9251ebba101508", users: ownerAddrs },
+        { role: "0x139c2898040ef16910dc9f44dc697df79363da767d8bc92f2e310312b816e46d", users: ownerAddrs },
       ],
       [
         [
-          800, // safetyFactor
+          600, // safetyFactor
           "0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E", // aaveSupplyToken
           "0x625E7708f30cA75bfd92586e17077590C60eb4cD", // aAaveSupplyToken
           "0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7", // aaveBorrowToken
@@ -118,13 +126,13 @@ async function getUsdcPortfolio() {
       [],
       0,
       [],
-      "0xce70b9444c4e22ae150C81dA7375542B49D15efA", // fee receiver
+      feeReceiver, // fee receiver
       [],
       BigInt(10 ** 20),
       BigInt(10 ** 20),
-      "0x736d964c70fF405e14aAc3094CeFe585812e1877", // Aave oracle
+      aaveOracleAddress, // Aave oracle
       0,
-      "0x60aE616a2155Ee3d9A68541Ba4544862310933d4", // swap service address
+      swapServiceAddress, // swap service address
       [],
       [],
       {}
@@ -146,7 +154,7 @@ async function getUsdcPortfolio() {
       [],
       0,
       [],
-      "0xce70b9444c4e22ae150C81dA7375542B49D15efA",
+      feeReceiver,
       [],
       BigInt(10 ** 20),
       BigInt(10 ** 20),
@@ -159,27 +167,27 @@ async function getUsdcPortfolio() {
     deployPortfolio(
       true,
       "PercentageAllocation",
-      "Real portfolio token",
-      "RPT1",
+      "Ambitious Portfolio Token",
+      "AMB",
       usdcContract,
       [wrapperPortfolio],
       0,
       [],
-      0,
+      500,
       [],
       0,
       [],
-      "0xce70b9444c4e22ae150C81dA7375542B49D15efA",
+      feeReceiver,
       [],
-      BigInt(10 ** 20),
-      BigInt(10 ** 20),
+      BigInt(10 ** 11),
+      BigInt(3 * 10 ** 9),
       [[100000]]
     )
   )
 }
 
 async function getUsdcStrategy() {
-  const OwnerAddrs = ["0xFf04026EaBb113A04878F8861902e533A51feb22", "0xeb1BF79Dfd7197308Ea045094217Bfc3c6aB9384"]
+  const ownerAddrs = ["0xFf04026EaBb113A04878F8861902e533A51feb22", "0xeb1BF79Dfd7197308Ea045094217Bfc3c6aB9384"]
 
   const usdcContract = await expectSuccess(getUsdcContract())
 
@@ -214,12 +222,12 @@ async function getUsdcStrategy() {
       0,
       "0x60aE616a2155Ee3d9A68541Ba4544862310933d4", // swap service address
       [
-        { role: "0x0000000000000000000000000000000000000000000000000000000000000000", users: OwnerAddrs },
-        { role: "0x7935bd0ae54bc31f548c14dba4d37c5c64b3f8ca900cb468fb8abd54d5894f55", users: OwnerAddrs },
-        { role: "0x17a8e30262c1f919c33056d877a3c22b95c2f5e4dac44683c1c2323cd79fbdb0", users: OwnerAddrs },
-        { role: "0x339759585899103d2ace64958e37e18ccb0504652c81d4a1b8aa80fe2126ab95", users: OwnerAddrs },
-        { role: "0x88aa719609f728b0c5e7fb8dd3608d5c25d497efbb3b9dd64e9251ebba101508", users: OwnerAddrs },
-        { role: "0x139c2898040ef16910dc9f44dc697df79363da767d8bc92f2e310312b816e46d", users: OwnerAddrs },
+        { role: "0x0000000000000000000000000000000000000000000000000000000000000000", users: ownerAddrs },
+        { role: "0x7935bd0ae54bc31f548c14dba4d37c5c64b3f8ca900cb468fb8abd54d5894f55", users: ownerAddrs },
+        { role: "0x17a8e30262c1f919c33056d877a3c22b95c2f5e4dac44683c1c2323cd79fbdb0", users: ownerAddrs },
+        { role: "0x339759585899103d2ace64958e37e18ccb0504652c81d4a1b8aa80fe2126ab95", users: ownerAddrs },
+        { role: "0x88aa719609f728b0c5e7fb8dd3608d5c25d497efbb3b9dd64e9251ebba101508", users: ownerAddrs },
+        { role: "0x139c2898040ef16910dc9f44dc697df79363da767d8bc92f2e310312b816e46d", users: ownerAddrs },
       ],
       [
         [
