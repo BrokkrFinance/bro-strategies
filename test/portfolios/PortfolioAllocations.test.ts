fed8e371decbddd23bbfa5b1e73c5d4f83987474
Jihoon Song <jihoonsong.dev@gmail.com>
2022-11-21 14:06:29 +0900

    Fix bug in portfolio allocation and elaborate it a bit

--- test/portfolios/PortfolioAllocations.test.ts
+++ test/portfolios/PortfolioAllocations.test.ts
@@ -4,10 +4,7 @@ export function testPortfolioAllocations() {
   describe("Allocations", async function () {
     it("should fail when the sum of target investable allocations is less than 100%", async function () {
       const investableLength = (await this.portfolio.getInvestables()).length
-      let allocations: number[] = [99000]
-      for (let i = 1; i < investableLength; i++) {
-        allocations.push(0)
-      }
+      const allocations: number[] = [99000].concat(Array<number>(investableLength - 1).fill(0))
 
       await expect(
         this.portfolio.connect(this.owner).setTargetInvestableAllocations(allocations)
@@ -16,10 +13,7 @@ export function testPortfolioAllocations() {
 
     it("should fail when the sum of target investable allocations is bigger than 100%", async function () {
       const investableLength = (await this.portfolio.getInvestables()).length
-      let allocations: number[] = [101000]
-      for (let i = 1; i < investableLength; i++) {
-        allocations.push(0)
-      }
+      const allocations: number[] = [101000].concat(Array<number>(investableLength - 1).fill(0))
 
       await expect(
         this.portfolio.connect(this.owner).setTargetInvestableAllocations(allocations)
@@ -28,22 +22,26 @@ export function testPortfolioAllocations() {
 
     it("should fail when the length of target investable allocations is shorter than the length of investables", async function () {
       const investableLength = (await this.portfolio.getInvestables()).length
-      let allocations: number[] = [100000]
-      for (let i = 1; i < investableLength - 1; i++) {
-        allocations.push(0)
+
+      let allocations: number[]
+      let customErrorName: string
+
+      if (investableLength == 1) {
+        allocations = []
+        customErrorName = "RebalancePercentageNot100"
+      } else {
+        allocations = [100000].concat(Array<number>(investableLength - 2).fill(0))
+        customErrorName = "RebalanceIncorrectAllocationsLength"
       }
 
       await expect(
         this.portfolio.connect(this.owner).setTargetInvestableAllocations(allocations)
-      ).to.be.revertedWithCustomError(this.portfolio, "RebalanceIncorrectAllocationsLength")
+      ).to.be.revertedWithCustomError(this.portfolio, customErrorName)
     })
 
     it("should fail when the length of target investable allocations is longer than the length of investables", async function () {
       const investableLength = (await this.portfolio.getInvestables()).length
-      let allocations: number[] = [100000]
-      for (let i = 1; i < investableLength + 1; i++) {
-        allocations.push(0)
-      }
+      const allocations: number[] = [100000].concat(Array<number>(investableLength).fill(0))
 
       await expect(
         this.portfolio.connect(this.owner).setTargetInvestableAllocations(allocations)
@@ -52,10 +50,8 @@ export function testPortfolioAllocations() {
 
     it("should succeed when the sum of target investable allocations equals to 100% and the length of target investable allocations equals to the length of investables", async function () {
       const investableLength = (await this.portfolio.getInvestables()).length
-      let allocations: number[] = [100000]
-      for (let i = 1; i < investableLength; i++) {
-        allocations.push(0)
-      }
+      const allocations: number[] = [100000].concat(Array<number>(investableLength - 1).fill(0))
+
       expect(await this.portfolio.connect(this.owner).setTargetInvestableAllocations(allocations))
         .to.emit(this.portfolio, "TargetInvestableAllocationsSet")
         .withArgs(allocations)
