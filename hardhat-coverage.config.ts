5122fc738aa6f10026ec6111cc255156e9af9245
Jihoon Song <jihoonsong.dev@gmail.com>
2022-11-28 22:08:10 +0900

    Remove network config other than forked mainnet

---
+++ hardhat-coverage.config.ts
@@ -0,0 +1,65 @@
+import "@mangrovedao/hardhat-test-solidity"
+import "@nomicfoundation/hardhat-chai-matchers"
+import "@nomicfoundation/hardhat-network-helpers"
+import "@nomiclabs/hardhat-ethers"
+import "@nomiclabs/hardhat-etherscan"
+import "@openzeppelin/hardhat-defender"
+import "@openzeppelin/hardhat-upgrades"
+import "@typechain/hardhat"
+import * as dotenv from "dotenv"
+import "hardhat-contract-sizer"
+import "hardhat-change-network"
+import "hardhat-gas-reporter"
+import { HardhatUserConfig } from "hardhat/config"
+import "solidity-coverage"
+import blockNumber from "./constants/BlockNumber.json"
+
+import "./tasks/deploy"
+import "./tasks/upgrade"
+
+dotenv.config()
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.10",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1,
+      },
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: false,
+      blockGasLimit: 30_000_000,
+      forking: {
+        url: "https://api.avax.network/ext/bc/C/rpc",
+        enabled: true,
+        blockNumber: blockNumber.forkAt,
+      },
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  mocha: {
+    timeout: 120000,
+  },
+  contractSizer: {
+    alphaSort: true,
+    runOnCompile: true,
+    strict: true,
+  },
+}
+
+export default config
