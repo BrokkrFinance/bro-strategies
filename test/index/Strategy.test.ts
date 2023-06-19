5b9bf8834dc994ab8f0bfd849c76be2fcd7bdff6
Jihoon Song <jihoonsong@Jihoons-MacBook-Pro.local>
2023-06-19 15:35:11 +0900

    Opt-out rebalance test

--- test/index/Strategy.test.ts
+++ test/index/Strategy.test.ts
@@ -10,7 +10,6 @@ import { testStrategyComponent } from "./StrategyComponent.test"
 import { testStrategyDeposit } from "./StrategyDeposit.test"
 import { testStrategyOracle } from "./StrategyOracle.test"
 import { testStrategyPausable } from "./StrategyPausable.test"
-import { testStrategyRebalance } from "./StrategyRebalance.test"
 import { testStrategyUpgradeable } from "./StrategyUpgradeable.test"
 import { testStrategyWithdraw } from "./StrategyWithdraw.test"
 
@@ -105,7 +104,7 @@ export function testStrategy(
     // testStrategyLimit() // Opt-out investment limit test because it fails when the limit is way higher than component's pool depth.
     testStrategyOracle()
     testStrategyPausable()
-    testStrategyRebalance()
+    // testStrategyRebalance() // Opt-out rebalance test because running it with arbitrary parameters is not meaningful.
     testStrategyUpgradeable()
     testStrategyWithdraw()
 
